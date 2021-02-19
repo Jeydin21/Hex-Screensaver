7ddf4b429b99cc3633ba9497de3f7398f70dc35c
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2021-02-19 12:42:42 -0600

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+function updateScreen() {
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
+	var s = d.getSeconds();
+
+	if (h < 10) { h = "0" + h; }
+	if (m < 10) { m = "0" + m; }
+	if (s < 10) { s = "0" + s; }
+
+	var hex = "#" + h + m + s;
+
+	document.getElementById("time").innerHTML = (h + " : " + m + " : " + s);
+	document.getElementById("hex").innerHTML = hex;
+	document.body.style.background = hex;
+
+	octo1.style.fill = hex;
+	octo2.style.fill = hex;
+}
+
+window.onload = setInterval(updateScreen, 100);
